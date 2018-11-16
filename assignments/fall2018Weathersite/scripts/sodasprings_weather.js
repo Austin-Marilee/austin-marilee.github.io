ccfcd9c33fb28b0cc6a8a1938d1bb7f5e154bc54
Austin-Marilee <marilee.austin@gmail.com>
2018-11-16 09:08:49 -0700

    Weather Site 2018 updates

--- assignments/fall2018Weathersite/scripts/sodasprings_weather.js
+++ assignments/fall2018Weathersite/scripts/sodasprings_weather.js
@@ -1,7 +1,7 @@
 var weatherObject = new XMLHttpRequest
-weatherObject.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5678757&appid=1b85c554f737909311537328716a5d15&units=imperial', true);
+weatherObject.open('GET', '//api.openweathermap.org/data/2.5/weather?id=5607916&appid=1b85c554f737909311537328716a5d15&units=imperial', true);
 
-//Franklin VA  4759986
+//Soda Springs 5607916
 
 weatherObject.send();
 
@@ -31,7 +31,7 @@ weatherObject.onload = function () {
 
 //forecast
 var weatherForecast = new XMLHttpRequest
-weatherForecast.open('GET', '//api.openweathermap.org/data/2.5/forecast?id=5678757&appid=1b85c554f737909311537328716a5d15&units=imperial', true);
+weatherForecast.open('GET', '//api.openweathermap.org/data/2.5/forecast?id=5607916&appid=1b85c554f737909311537328716a5d15&units=imperial', true);
 
 
 weatherForecast.send();
