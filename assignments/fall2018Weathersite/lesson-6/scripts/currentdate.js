bc1ec689fad4b6ae6e5d82baf3450cff77f5dc1f
Austin-Marilee <marilee.austin@gmail.com>
2019-02-12 19:11:50 -0700

    minimized date

--- assignments/fall2018Weathersite/lesson-6/scripts/currentdate.js
+++ assignments/fall2018Weathersite/lesson-6/scripts/currentdate.js
@@ -1,13 +1,8 @@
-window.onload=function() {
-
 var d = new Date();
-var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var n = weekday[d.getDay()];
-var m = month[d.getMonth()];
+document.getElementById("currentdate").innerHTML = weekday[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
 
-document.getElementById("currentdate").innerHTML = n + ", " + d.getDate() + " " + m + " " + d.getFullYear();
 
-}
