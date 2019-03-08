efa8ca658e35026c00df2462786c23ef4d40f29d
Austin-Marilee <marilee.austin@gmail.com>
2019-03-07 20:55:15 -0700

    home page image typos and start of lesson 10

--- assignments/fall2018Weathersite/lesson-10/scripts/preston.js
+++ assignments/fall2018Weathersite/lesson-10/scripts/preston.js
@@ -1,4 +1,6 @@
-//PRESTON IDAHO 5061036
+//PRESTON IDAHO 5604473
+
+//WEATHER SUMMARY
 var weatherObject = new XMLHttpRequest
 weatherObject.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=1b85c554f737909311537328716a5d15&units=imperial', true);
 weatherObject.send();
@@ -7,14 +9,12 @@ weatherObject.onload = function () {
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
 
-    //WEATHER SUMMARY
     document.getElementById('weatherDesc').innerHTML = weatherInfo.weather[0].main;
     document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
     document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 
     var windChill = 35.74 + 0.6215 * weatherInfo.main.temp - 35.75 * Math.pow(weatherInfo.wind.speed, 0.16) + 0.4275 * weatherInfo.main.temp * Math.pow(weatherInfo.wind.speed, 0.16);
-
     windChill = Math.round(windChill);
     document.getElementById("windChill").innerHTML = windChill;
 }
@@ -27,8 +27,6 @@ weatherForecast.onload = function () {
 
     var weatherInfo = JSON.parse(weatherForecast.responseText);
     console.log(weatherInfo);
-
-    //DATE
 
     var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -53,26 +51,48 @@ weatherForecast.onload = function () {
     document.getElementById('dayfour').innerHTML = m4 + ' ' + date4.getDate();
     document.getElementById('dayfive').innerHTML = m5 + ' ' + date5.getDate();
 
-    //HIGH TEMPERATURE
 
-    var i;
-    var temp = "";
-    var n = weatherInfo.list[5].dt_txt;
-    for (var i = 0; i < weatherInfo.list[i].length; i++) {
- 
-        if (n.includes("15:00:00") = true) {
-        temp += weatherInfo.list[i].main.temp_max;
+    var index, time, temp;
+    for (index = 0; index < weatherInfo.list.length; ++index) {
+        time = weatherInfo.list[index].dt_txt;
+        if (time.includes("15:00:00")) {
+            var temp = weatherInfo.list[index].main.temp_max;
+            var listTemp = [temp];
+            for (i = 1; i < listTemp.length; ++i) {
+                listTemp.push(temp);
+                break;
+                }
+
         }
-        document.getElementById("dayonetemp").innerHTML = temp;
+        continue;
     }
 
+        document.getElementById("highTemp1").innerHTML = listTemp[0];
+        document.getElementById("highTemp2").innerHTML = listTemp[1];
+        document.getElementById("highTemp3").innerHTML = listTemp[2];
+        document.getElementById("highTemp4").innerHTML = listTemp[3];
+        document.getElementById("highTemp5").innerHTML = listTemp[4];
 
 
-    /*     document.getElementById('dayonetemp').innerHTML = weatherInfo.list[6].main.temp_max;
-        document.getElementById('daytwotemp').innerHTML = weatherInfo.list[14].main.temp_max;
-        document.getElementById('daythreetemp').innerHTML = weatherInfo.list[22].main.temp_max;
-        document.getElementById('dayfourtemp').innerHTML = weatherInfo.list[30].main.temp_max;
-        document.getElementById('dayfivetemp').innerHTML = weatherInfo.list[38].main.temp_max; */
+
+/*     function myFunction() {
+        var list = document.getElementsByClassName("example")[0];
+        list.getElementsByClassName("child")[0].innerHTML = "Milk";
+
+
+            for (var i = 0; i < result.length; i++) {
+                result[i].innerHTML = temp;
+
+
+
+      } */
+
+
+    /*     document.getElementById('dayonetemp').innerHTML = weatherInfo.list[3].main.temp_max;
+        document.getElementById('daytwotemp').innerHTML = weatherInfo.list[11].main.temp_max;
+        document.getElementById('daythreetemp').innerHTML = weatherInfo.list[19].main.temp_max;
+        document.getElementById('dayfourtemp').innerHTML = weatherInfo.list[27].main.temp_max;
+        document.getElementById('dayfivetemp').innerHTML = weatherInfo.list[35].main.temp_max; */
 
     //icons
 
@@ -92,7 +112,7 @@ weatherForecast.onload = function () {
     var icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
     document.getElementById('weather_iconfour').src = icon_path;
 
-    var iconcode = weatherInfo.list[38].weather["0"].icon;
+    var iconcode = weatherInfo.list[37].weather["0"].icon;
     var icon_path = "https://openweathermap.org/img/w/" + iconcode + ".png";
     document.getElementById('weather_iconfive').src = icon_path;
 }
