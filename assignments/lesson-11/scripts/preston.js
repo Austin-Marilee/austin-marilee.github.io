03d97cb7c68a90c62d718e10abf4841f7efdba85
Austin-Marilee <marilee.austin@gmail.com>
2019-03-15 10:04:44 -0600

    speed updates

--- assignments/lesson-11/scripts/preston.js
+++ assignments/lesson-11/scripts/preston.js
@@ -14,7 +14,7 @@ weatherObject.onload = function () {
     document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 
-    var windChill = 35.74 + 0.6215 * weatherInfo.main.temp - 35.75 * Math.pow(weatherInfo.wind.speed, 0.16) + 0.4275 * weatherInfo.main.temp * Math.pow(weatherInfo.wind.speed, 0.16);
+var windChill = 35.74 + 0.6215 * weatherInfo.main.temp - 35.75 * Math.pow(weatherInfo.wind.speed, 0.16) + 0.4275 * weatherInfo.main.temp * Math.pow(weatherInfo.wind.speed, 0.16);
     windChill = Math.round(windChill);
     document.getElementById("windChill").innerHTML = windChill;
 }
